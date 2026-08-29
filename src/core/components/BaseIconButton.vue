c949fa48cff8fe77f9ed90e1692e91f43ff395d9
VictorSoares98 <luan.victtor@hotmail.com>
2026-08-29 19:24:50 -0300

    Summary: Estender BaseIconButton com propriedades de cor, estado e título
    Details:
    - BaseIconButton.vue: Adicionadas as props color, disable e title ao componente
    
    Effects:
    - Permite maior customização visual e controle de estado do botão
    - Melhora a acessibilidade e a experiência do usuário com o atributo title

--- src/core/components/BaseIconButton.vue
+++ src/core/components/BaseIconButton.vue
@@ -2,11 +2,23 @@
 defineProps<{
   icon: string;
   ariaLabel: string;
+  color?: string;
+  disable?: boolean;
+  title?: string;
 }>();
 </script>
 
 <template>
-  <q-btn flat round :icon="icon" :aria-label="ariaLabel" class="tw-min-w-[44px] tw-min-h-[44px]">
+  <q-btn
+    flat
+    round
+    :icon="icon"
+    :aria-label="ariaLabel"
+    :color="color"
+    :disable="disable"
+    :title="title"
+    class="tw-min-w-[44px] tw-min-h-[44px]"
+  >
     <slot></slot>
   </q-btn>
 </template>
